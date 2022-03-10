131ab39988e9c7714980edfb59b508f8a8cc1cfd
houseofsam <samxsaid@gmail.com>
2022-03-09 23:12:09 -0500

    Modify Footer component markup

--- components/Footer.js
+++ components/Footer.js
@@ -1,107 +1,103 @@
-import Image from "next/image";
+import Logo from "./Header/Logo";
+import PinDropOutlinedIcon from "@mui/icons-material/PinDropOutlined";
+import LocalPhoneOutlinedIcon from "@mui/icons-material/LocalPhoneOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 
 const Footer = () => {
   return (
     <footer>
-      <div className="container">
+      <div className="footer-columns">
         {/* <!-- ABOUT Location --> */}
-        <div className="col-md-3">
-          {/* <div className="about-footer"> <Image className="margin-bottom-30" src="images/logo-foot.png" alt="" /> */}
+        <div className="footer-column footer-column-1">
+          <Logo />
           <p>
-            <i className="icon-pointer"></i> Street No. 12, Newyork 12, <br />
+            <PinDropOutlinedIcon /> Street No. 12, Newyork 12, <br />
             MD - 123, USA.
           </p>
           <p>
-            <i className="icon-call-end"></i> 1.800.123.456789
+            <LocalPhoneOutlinedIcon /> 1.800.123.456789
           </p>
           <p>
-            <i className="icon-envelope"></i> info@ecoshop.com
+            <EmailOutlinedIcon /> info@shop.com
           </p>
         </div>
-      </div>
 
-      {/* <!-- HELPFUL LINKS --> */}
-      <div className="col-md-3">
-        <h6>HELPFUL LINKS</h6>
-        <ul className="link">
-          <li>
-            <a href="#."> Products</a>
-          </li>
-          <li>
-            <a href="#."> Find a Store</a>
-          </li>
-          <li>
-            <a href="#."> Features</a>
-          </li>
-          <li>
-            <a href="#."> Privacy Policy</a>
-          </li>
-          <li>
-            <a href="#."> Blog</a>
-          </li>
-          <li>
-            <a href="#."> Press Kit </a>
-          </li>
-        </ul>
-      </div>
+        {/* <!-- HELPFUL LINKS --> */}
+        <div className="footer-column footer-column-2">
+          <h6>HELPFUL LINKS</h6>
+          <ul className="footer-links">
+            <li>
+              <a href="#."> Products</a>
+            </li>
+            <li>
+              <a href="#."> Find a Store</a>
+            </li>
+            <li>
+              <a href="#."> Features</a>
+            </li>
+            <li>
+              <a href="#."> Privacy Policy</a>
+            </li>
+            <li>
+              <a href="#."> Blog</a>
+            </li>
+            <li>
+              <a href="#."> Press Kit </a>
+            </li>
+          </ul>
+        </div>
 
-      {/* <!-- SHOP --> */}
-      <div className="col-md-3">
-        <h6>SHOP</h6>
-        <ul className="link">
-          <li>
-            <a href="#."> About Us</a>
-          </li>
-          <li>
-            <a href="#."> Career</a>
-          </li>
-          <li>
-            <a href="#."> Shipping Methods</a>
-          </li>
-          <li>
-            <a href="#."> Contact</a>
-          </li>
-          <li>
-            <a href="#."> Support</a>
-          </li>
-          <li>
-            <a href="#."> Retailer</a>
-          </li>
-        </ul>
-      </div>
+        {/* <!-- SHOP --> */}
+        <div className="footer-column footer-column-3">
+          <h6>SHOP</h6>
+          <ul className="footer-links">
+            <li>
+              <a href="#."> About Us</a>
+            </li>
+            <li>
+              <a href="#."> Career</a>
+            </li>
+            <li>
+              <a href="#."> Shipping Methods</a>
+            </li>
+            <li>
+              <a href="#."> Contact</a>
+            </li>
+            <li>
+              <a href="#."> Support</a>
+            </li>
+            <li>
+              <a href="#."> Retailer</a>
+            </li>
+          </ul>
+        </div>
 
-      {/* <!-- MY ACCOUNT --> */}
-      <div className="col-md-3">
-        <h6>MY ACCOUNT</h6>
-        <ul className="link">
-          <li>
-            <a href="#."> Login</a>
-          </li>
-          <li>
-            <a href="#."> My Account</a>
-          </li>
-          <li>
-            <a href="#."> My Cart</a>
-          </li>
-          <li>
-            <a href="#."> Wishlist</a>
-          </li>
-          <li>
-            <a href="#."> Checkout</a>
-          </li>
-        </ul>
+        {/* <!-- MY ACCOUNT --> */}
+        <div className="footer-column footer-column-4">
+          <h6>MY ACCOUNT</h6>
+          <ul className="footer-links">
+            <li>
+              <a href="#."> Login</a>
+            </li>
+            <li>
+              <a href="#."> My Account</a>
+            </li>
+            <li>
+              <a href="#."> My Cart</a>
+            </li>
+            <li>
+              <a href="#."> Wishlist</a>
+            </li>
+            <li>
+              <a href="#."> Checkout</a>
+            </li>
+          </ul>
+        </div>
       </div>
 
       {/* <!-- Rights --> */}
-
       <div className="rights">
-        <p>© 2016 ecoshop All right reserved. </p>
-        <div className="scroll">
-          {" "}
-          <a href="#wrap" className="go-up">
-            <i className="lnr lnr-arrow-up"></i>
-          </a>{" "}
-        </div>
+        <p>© 2022 All Rights Reserved.</p>
       </div>
     </footer>
   );
