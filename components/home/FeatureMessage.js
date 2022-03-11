1d823e587fc3fb8e6b0c39e8e4e3c66138754db2
houseofsam <samxsaid@gmail.com>
2022-03-10 23:50:43 -0500

    create FeatureMessage component for home page and import it to index.js

---
+++ components/home/FeatureMessage.js
@@ -0,0 +1,10 @@
+const FeatureMessage = () => {
+  return ( 
+    <div className="feature-message">
+      <h2>New Arrival</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec faucibus maximus vehicula. Sed feugiat, tellus vel tristique posuere, diam</p>
+    </div>
+  );
+}
+ 
+export default FeatureMessage;
