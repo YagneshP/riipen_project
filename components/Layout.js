b26642c2a7dcff7317181735f6c4b70b1d97554b
raninder dhillon <raninderdhillon@gmail.com>
2022-03-12 13:02:40 -0500

    write component added to layout and .env file created

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,16 @@
 import Footer from './Footer';
 import Header from './Header';
 import About from '../pages/about';
+import WriteToCloudFirestore from './firebase/write';
 
 const Layout = ({ children }) => {
   return ( 
     <div className="content">
-      <Header />
+      <WriteToCloudFirestore />
+      {/* <Header /> */}
       {/* <About /> */}
-        { children }
-      <Footer />
+        {/* { children } */}
+      {/* <Footer /> */}
     </div>
    );
 }
