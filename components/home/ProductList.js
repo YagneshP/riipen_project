358802865bca63c1cf0b418e04d4062bcb06e186
houseofsam <samxsaid@gmail.com>
2022-03-14 17:24:28 -0400

    Create ProductList & ProductListItem components for New Arrivals section

---
+++ components/home/ProductList.js
@@ -0,0 +1,61 @@
+import ProductListItem from "./ProductListItem";
+
+const ProductList = () => {
+  const mockData = [
+    {
+      id: 1,
+      itemName: "Chanel",
+      itemDesc: "Placeholder text. Item description goes here for new arrivals",
+      image: "https://placeimg.com/360/360/any/grayscale"
+    },
+    {
+      id: 2,
+      itemName: "YSL",
+      itemDesc: "Placeholder text. Item description goes here for new arrivals",
+      image: "https://placeimg.com/360/360/any/grayscale"
+    },
+    {
+      id: 3,
+      itemName: "Armani",
+      itemDesc: "Placeholder text. Item description goes here for new arrivals",
+      image: "https://placeimg.com/360/360/any/grayscale"
+    },
+    {
+      id: 4,
+      itemName: "Hugo Boss",
+      itemDesc: "Placeholder text. Item description goes here for new arrivals",
+      image: "https://placeimg.com/360/360/any/grayscale"
+    },
+    {
+      id: 4,
+      itemName: "Gucci",
+      itemDesc: "Placeholder text. Item description goes here for new arrivals",
+      image: "https://placeimg.com/360/360/any/grayscale"
+    },
+    {
+      id: 4,
+      itemName: "Dior",
+      itemDesc: "Placeholder text. Item description goes here for new arrivals",
+      image: "https://placeimg.com/360/360/any/grayscale"
+    }
+  ];
+
+  const mappedItems = mockData.map((item) => {
+    return (
+      <ProductListItem
+        key={item.id}
+        itemName={item.itemName}
+        itemDesc={item.itemDesc}
+        image={item.image}
+      />
+    )
+  });
+
+  return ( 
+    <div className="new-arrival-products">
+      {mappedItems}
+    </div>
+   );
+}
+ 
+export default ProductList;
