f693e3f12624e2fc4225bd5cf07d122a99122da4
tofi-124 <tofikahm@yahoo.com>
2022-03-14 15:52:45 -0400

    reseting the form to build using form spree format

--- components/Contact/index.js
+++ components/Contact/index.js
@@ -3,8 +3,10 @@ import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
 import EmailIcon from "@mui/icons-material/Email";
 import { useEffect, useRef } from "react";
 import { Loader } from "@googlemaps/js-api-loader";
+import { useForm, ValidationError } from "@formspree/react";
 
 const Contact = () => {
+  //Google Map setup
   const googlemap = useRef(null);
   useEffect(() => {
     const loader = new Loader({
@@ -15,108 +17,25 @@ const Contact = () => {
     let map;
     loader.load().then(() => {
       map = new google.maps.Map(googlemap.current, {
-        center: { lat: 43.651890, lng: -79.381706 },
+        center: { lat: 43.65189, lng: -79.381706 },
         zoom: 8,
       });
     });
   });
 
-  return (
-    <div>
-      <section className="contact padding-top-100 padding-bottom-100">
-        <div className="container">
-          <div className="contact-form">
-            <h5>PLEASE fill-up FEW details</h5>
-            <div className="row">
-              <div className="col-md-8">
-                <div id="contact_message" className="success-msg">
-                  {" "}
-                  <i className="fa fa-paper-plane-o"></i>Thank You. Your Message
-                  has been Submitted
-                </div>
+  // Form spree setup
+  const [state, handleSubmit] = useForm("mpzblqyb");
 
-                <form
-                  role="form"
-                  id="contact_form"
-                  className="contact-form"
-                  method="post"
-                  onSubmit="return false"
-                >
-                  <ul className="row">
-                    <li className="col-sm-6">
-                      <label>
-                        full name *
-                        <input
-                          type="text"
-                          className="form-control"
-                          name="name"
-                          id="name"
-                          placeholder=""
-                        />
-                      </label>
-                    </li>
-                    <li className="col-sm-6">
-                      <label>
-                        Email *
-                        <input
-                          type="text"
-                          className="form-control"
-                          name="email"
-                          id="email"
-                          placeholder=""
-                        />
-                      </label>
-                    </li>
-                    <li className="col-sm-6">
-                      <label>
-                        Phone *
-                        <input
-                          type="text"
-                          className="form-control"
-                          name="company"
-                          id="company"
-                          placeholder=""
-                        />
-                      </label>
-                    </li>
-                    <li className="col-sm-6">
-                      <label>
-                        SUBJECT
-                        <input
-                          type="text"
-                          className="form-control"
-                          name="website"
-                          id="website"
-                          placeholder=""
-                        />
-                      </label>
-                    </li>
-                    <li className="col-sm-12">
-                      <label>
-                        Message
-                        <textarea
-                          className="form-control"
-                          name="message"
-                          id="message"
-                          rows="5"
-                          placeholder=""
-                        ></textarea>
-                      </label>
-                    </li>
-                    <li className="col-sm-12">
-                      <button
-                        type="submit"
-                        value="submit"
-                        className="button-12"
-                        id="btn_submit"
-                        onClick="proceed();"
-                      >
-                        SEND MAIL
-                      </button>
-                    </li>
-                  </ul>
-                </form>
-              </div>
+  if (state.succeeded) {
+    <div id="contact_message" className="success-msg">
+      {" "}
+      <i className="fa fa-paper-plane-o"></i>Thank You. Your Message has been
+      Submitted
+    </div>;
+  }
+
+  return (
+    
 
               <div className="col-md-4">
                 <div className="contact-info">
