658a13e8e0cc9d9fa987112fc937b9719769a0bd
houseofsam <samxsaid@gmail.com>
2022-03-13 20:50:44 -0400

    convert home page banner to carousel

--- components/Header/Banner.js
+++ components/Header/Banner.js
@@ -1,16 +1,51 @@
 import Image from 'next/image';
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Banner = () => {
   return ( 
     <div className="banner-container">
-      <Image
-        className="banner-image-1"
-        src="/images/fragrance-banner.png"
-        alt="Banner image"
-        layout="responsive"
-        width="100%"
-        height="35vh"
-      />
+      <Carousel
+        autoPlay={true}
+        infiniteLoop={true} 
+        swipeable={true}
+        stopOnHover={true}
+        transitionTime={800}
+        showStatus={false}
+        emulateTouch={true}
+        showThumbs={false}
+      >
+        <div>
+          <Image
+            className="banner-image-1"
+            src="/images/fragrance-banner.png"
+            alt="Banner image"
+            layout="responsive"
+            width="100%"
+            height="35vh"
+          />
+        </div>
+        <div>
+          <Image
+            className="banner-image-1"
+            src="/images/fragrance-banner.png"
+            alt="Banner image"
+            layout="responsive"
+            width="100%"
+            height="35vh"
+          />
+        </div>
+        <div>
+          <Image
+            className="banner-image-1"
+            src="/images/fragrance-banner.png"
+            alt="Banner image"
+            layout="responsive"
+            width="100%"
+            height="35vh"
+          />
+        </div>
+      </Carousel>
     </div>
    );
 }
