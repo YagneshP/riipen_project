e0915c7e4467bad30c6ca4d56ad3397b56b2f954
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-03-15 09:13:53 -0400

    style the register form

--- pages/register.js
+++ pages/register.js
@@ -37,11 +37,15 @@ export default function Register() {
           <h6>REGISTER</h6>
           <form onSubmit={handleSubmit}>
             <ul className='row'>
-              <li className='col-md-6'>
-                <label htmlFor='first-name'> *FIRST NAME </label>
+              <li className='col-md-6 mb-3'>
+                <label className='form-label' htmlFor='first-name'>
+                  {" "}
+                  *FIRST NAME{" "}
+                </label>
                 <input
                   type='text'
                   name='first-name'
+                  className='form-control'
                   value={firstName}
                   placeholder='John'
                   onChange={(e) => setFirstName(e.target.value)}
@@ -49,23 +53,31 @@ export default function Register() {
                 />
               </li>
 
-              <li className='col-md-6'>
-                <label htmlFor='last-name'> *LAST NAME </label>
+              <li className='col-md-6 mb-3'>
+                <label className='form-label' htmlFor='last-name'>
+                  {" "}
+                  *LAST NAME{" "}
+                </label>
                 <input
                   type='text'
                   name='last-name'
                   value={lastName}
                   placeholder='Doe'
+                  className='form-control'
                   onChange={(e) => setLastName(e.target.value)}
                 />
               </li>
 
-              <li className='col-md-6'>
-                <label htmlFor='contry-state'> *EMAIL ADDRESS</label>
+              <li className='col-md-6 mb-3'>
+                <label className='form-label' htmlFor='contry-state'>
+                  {" "}
+                  *EMAIL ADDRESS
+                </label>
                 <input
                   type='email'
                   name='contry-state'
                   value={email}
+                  className='form-control'
                   placeholder='john@email.com'
                   onChange={(e) => setEmail(e.target.value)}
                   required
@@ -73,36 +85,43 @@ export default function Register() {
               </li>
 
               {/* <li className="col-md-6">
-            <label htmlFor='phone'> *PHONE </label>
+            <label className="form-label" htmlFor='phone'> *PHONE </label>
             <input type='text' name='phone' value='' placeholder='' />
           </li> */}
-              <li className='col-md-6'>
-                <label htmlFor='password'>*PASSWORD</label>
+              <li className='col-md-6 mb-3'>
+                <label className='form-label' htmlFor='password'>
+                  *PASSWORD
+                </label>
                 <input
                   type='password'
                   name='password'
                   value={password}
+                  className='form-control'
                   onChange={(e) => setPassword(e.target.value)}
                   placeholder=''
                 />
               </li>
-              <li className='col-md-6'>
-                <label htmlFor='re-password'> *REPEAT PASSWORD</label>
+              <li className='col-md-6 mb-3'>
+                <label className='form-label' htmlFor='re-password'>
+                  {" "}
+                  *REPEAT PASSWORD
+                </label>
                 <input
                   type='password'
                   name='re-password'
                   value={rePassword}
+                  className='form-control'
                   onChange={(e) => setRePassword(e.target.value)}
                   placeholder=''
                 />
               </li>
               {/* <li className="col-md-6">
-            <label htmlFor='address-1'>*ADDRESS </label>
+            <label className="form-label" htmlFor='address-1'>*ADDRESS </label>
 
             <input type='text' name='address-1' value='' placeholder='' />
           </li>
           <li className="col-md-6">
-            <label htmlFor='address-2'> *ADDRESS</label>
+            <label className="form-label" htmlFor='address-2'> *ADDRESS</label>
 
             <input type='text' name='address-2' value='' placeholder='' />
           </li> */}
@@ -127,7 +146,7 @@ export default function Register() {
           </li> */}
 
               <li>
-                <button type='submit' className='btn'>
+                <button type='submit' className='btn mb-3'>
                   REGISTER NOW
                 </button>
               </li>
