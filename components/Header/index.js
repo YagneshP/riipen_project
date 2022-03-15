7460a8badd12668faf0ba04d25e469944fe0485a
houseofsam <samxsaid@gmail.com>
2022-03-15 00:47:02 -0400

    Add logic that listens to viewport width & modifies state for nav conditional rendering

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,11 +1,33 @@
+import { useState, useEffect } from "react";
 import Logo from "./Logo";
+import MobileMenu from "./MobileMenu";
 import Nav from "./Nav";
 
 const Header = () => {
+  const [isDesktop, setDesktop] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 767) {
+      setDesktop(true);
+    } else {
+      setDesktop(false);
+    }
+
+    const updateMedia = () => {
+      if (window.innerWidth > 767) {
+        setDesktop(true);
+      } else {
+        setDesktop(false);
+      }
+    };
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  }, []);
+
   return (
     <header className="header">
       <Logo />
-      <Nav />
+      {isDesktop ? <Nav /> : <MobileMenu />}
     </header>
   );
 };
