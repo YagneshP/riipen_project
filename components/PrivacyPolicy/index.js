4e318b140e0f58f0fe6b60e83cdb5a5c5b744963
tofi-124 <tofikahm@yahoo.com>
2022-03-18 18:05:50 -0400

    privacy policy section updated

--- components/PrivacyPolicy/index.js
+++ components/PrivacyPolicy/index.js
@@ -102,11 +102,11 @@ const PrivacyPolicy = () => {
                             or the purpose of the CCPA (Canada Consumer Privacy
                             Act), means a natural person who is a Canada
                             resident. A resident, as defined in the law,
-                            includes (1) every individual who is in the USA for
-                            other than a temporary or transitory purpose, and
-                            (2) every individual who is domiciled in the USA who
-                            is outside the USA for a temporary or transitory
-                            purpose.
+                            includes (1) every individual who is in the Canada
+                            for other than a temporary or transitory purpose,
+                            and (2) every individual who is domiciled in the
+                            Canada who is outside the Canada for a temporary or
+                            transitory purpose.
                           </li>
                           <br />
                           <li>
@@ -204,97 +204,233 @@ const PrivacyPolicy = () => {
                           </li>
                           <br />
                         </ul>
+                        <ul className="column">
+                          <div className="title">
+                            Collecting and Using Your Personal Data
+                          </div>
+                          <div className="title">Types of Data Collected</div>
+                          <div className="title">Personal Data</div>
+                          <br />
+                          <li>
+                            <div className="bold">
+                              To provide and maintain our Service :
+                            </div>
+                            including to monitor the usage of our Service.
+                          </li>
+                          <br />
+                          <li>
+                            <div className="bold">To manage Your Account :</div>
+                            to manage Your registration as a user of the
+                            Service. The Personal Data You provide can give You
+                            access to different functionalities of the Service
+                            that are available to You as a registered user.
+                          </li>
+                          <br />
+                          <li>
+                            <div className="bold">
+                              For the performance of a contract:
+                            </div>
+                            the development, compliance and undertaking of the
+                            purchase contract for the products, items or
+                            services You have purchased or of any other contract
+                            with Us through the Service.
+                          </li>
+                          <br />
+                          <li>
+                            <div className="bold">To contact You:</div> To
+                            contact You by email, telephone calls, SMS, or other
+                            equivalent forms of electronic communication, such
+                            as a mobile application's push notifications
+                            regarding updates or informative communications
+                            related to the functionalities, products or
+                            contracted services, including the security updates,
+                            when necessary or reasonable for their
+                            implementation.
+                          </li>
+                          <br />
+                          <li>
+                            <div className="bold">
+                              To provide You with news :
+                            </div>
+                            special offers and general information about other
+                            goods, services and events which we offer that are
+                            similar to those that you have already purchased or
+                            enquired about unless You have opted not to receive
+                            such information.
+                          </li>
+                          <br />
+                          <li>
+                            <div className="bold">
+                              To manage Your requests :
+                            </div>
+                            To attend and manage Your requests to Us.
+                          </li>
+                          <br />
+                          <li>
+                            <div className="bold">For business transfers :</div>
+                            We may use Your information to evaluate or conduct a
+                            merger, divestiture, restructuring, reorganization,
+                            dissolution, or other sale or transfer of some or
+                            all of Our assets, whether as a going concern or as
+                            part of bankruptcy, liquidation, or similar
+                            proceeding, in which Personal Data held by Us about
+                            our Service users is among the assets transferred.
+                          </li>
+                          <br />
+                          <li>
+                            <div className="bold">For other purposes :</div> We
+                            may use Your information for other purposes, such as
+                            data analysis, identifying usage trends, determining
+                            the effectiveness of our promotional campaigns and
+                            to evaluate and improve our Service, products,
+                            services, marketing and your experience.
+                          </li>
+                          <br />
+                        </ul>
+                        We may share Your personal information in the following
+                        situations:
+                        <br />
+                        <br />
+                        <ul className="column">
+                          <li>
+                            <div className="bold">With Service Providers :</div>
+                            We may share Your personal information with Service
+                            Providers to monitor and analyze the use of our
+                            Service, for payment processing, to contact You.
+                          </li>
+                          <br />
+                          <li>
+                            <div className="bold">For business transfers :</div>
+                            We may share or transfer Your personal information
+                            in connection with, or during negotiations of, any
+                            merger, sale of Company assets, financing, or
+                            acquisition of all or a portion of Our business to
+                            another company.
+                          </li>
+                          <br />
+                          <li>
+                            <div className="bold">With Affiliates :</div> We may
+                            share Your information with Our affiliates, in which
+                            case we will require those affiliates to honor this
+                            Privacy Policy. Affiliates include Our parent
+                            company and any other subsidiaries, joint venture
+                            partners or other companies that We control or that
+                            are under common control with Us.
+                          </li>
+                          <br />
+                          <li>
+                            <div className="bold">With business partners :</div>
+                            We may share Your information with Our business
+                            partners to offer You certain products, services or
+                            promotions.
+                          </li>
+                          <br />
+                          <li>
+                            <div className="bold">With other users :</div> when
+                            You share personal information or otherwise interact
+                            in the public areas with other users, such
+                            information may be viewed by all users and may be
+                            publicly distributed outside.
+                          </li>
+                          <br />
+                          <li>
+                            <div className="bold">With Your consent :</div> We
+                            may disclose Your personal information for any other
+                            purpose with Your consent.
+                          </li>
+                          <br />
+                        </ul>
+                        <br />
                         <div className="title">
-                          Collecting and Using Your Personal Data
+                          Retention of Your Personal Data
                         </div>
-                        <div className="title">Types of Data Collected</div>
-                        <div className="title">Personal Data</div>
                         <br />
-                        <li>
-                          <div className="bold">
-                            To provide and maintain our Service :
-                          </div>
-                          including to monitor the usage of our Service.
-                        </li>
+                        The Company will retain Your Personal Data only for as
+                        long as is necessary for the purposes set out in this
+                        Privacy Policy. We will retain and use Your Personal
+                        Data to the extent necessary to comply with our legal
+                        obligations (for example, if we are required to retain
+                        your data to comply with applicable laws), resolve
+                        disputes, and enforce our legal agreements and policies.
                         <br />
-                        <li>
-                          
-                          <div className="bold">
-                            To manage Your Account :
-                          </div>
-                          to manage Your registration as a user of the Service.
-                          The Personal Data You provide can give You access to
-                          different functionalities of the Service that are
-                          available to You as a registered user.
-                        </li>
                         <br />
-                        <li>
-                          
-                          <div className="bold">
-                            For the performance of a contract:
-                          </div>
-                          the development, compliance and undertaking of the
-                          purchase contract for the products, items or services
-                          You have purchased or of any other contract with Us
-                          through the Service.
-                        </li>
+                        The Company will also retain Usage Data for internal
+                        analysis purposes. Usage Data is generally retained for
+                        a shorter period of time, except when this data is used
+                        to strengthen the security or to improve the
+                        functionality of Our Service, or We are legally
+                        obligated to retain this data for longer time periods.
                         <br />
-                        <li>
-                          
-                          <div className="bold">To contact You:</div> To contact
-                          You by email, telephone calls, SMS, or other
-                          equivalent forms of electronic communication, such as
-                          a mobile application's push notifications regarding
-                          updates or informative communications related to the
-                          functionalities, products or contracted services,
-                          including the security updates, when necessary or
-                          reasonable for their implementation.
-                        </li>
                         <br />
-                        <li>
-                          
-                          <div className="bold">
-                            To provide You with news :
-                          </div>
-                          special offers and general information about other
-                          goods, services and events which we offer that are
-                          similar to those that you have already purchased or
-                          enquired about unless You have opted not to receive
-                          such information.
-                        </li>
+                        <div className="title">
+                          Transfer of Your Personal Data
+                        </div>
                         <br />
-                        <li>
-                          
-                          <div className="bold">
-                            To manage Your requests :
-                          </div>
-                          To attend and manage Your requests to Us.
-                        </li>
+                        Your information, including Personal Data, is processed
+                        at the Company's operating offices and in any other
+                        places where the parties involved in the processing are
+                        located. It means that this information may be
+                        transferred to — and maintained on — computers located
+                        outside of Your state, province, country or other
+                        governmental jurisdiction where the data protection laws
+                        may differ than those from Your jurisdiction. Your
+                        consent to this Privacy Policy followed by Your
+                        submission of such information represents Your agreement
+                        to that transfer.
                         <br />
-                        <li>
-                          
-                          <div className="bold">
-                            For business transfers :
-                          </div>
-                          We may use Your information to evaluate or conduct a
-                          merger, divestiture, restructuring, reorganization,
-                          dissolution, or other sale or transfer of some or all
-                          of Our assets, whether as a going concern or as part
-                          of bankruptcy, liquidation, or similar proceeding, in
-                          which Personal Data held by Us about our Service users
-                          is among the assets transferred.
-                        </li>
                         <br />
-                        <li>
-                          
-                          <div className="bold">For other purposes :</div> We
-                          may use Your information for other purposes, such as
-                          data analysis, identifying usage trends, determining
-                          the effectiveness of our promotional campaigns and to
-                          evaluate and improve our Service, products, services,
-                          marketing and your experience.
-                        </li>
+                        The Company will take all steps reasonably necessary to
+                        ensure that Your data is treated securely and in
+                        accordance with this Privacy Policy and no transfer of
+                        Your Personal Data will take place to an organization or
+                        a country unless there are adequate controls in place
+                        including the security of Your data and other personal
+                        information.
                         <br />
+                        <br />
+                        <div className="title">
+                          Disclosure of Your Personal Data
+                        </div>
+                        <br />
+                        <div className="bold">Business Transactions :</div>
+                        If the Company is involved in a merger, acquisition or
+                        asset sale, Your Personal Data may be transferred. We
+                        will provide notice before Your Personal Data is
+                        transferred and becomes subject to a different Privacy
+                        Policy.
+                        <br />
+                        <br />
+                        <div className="bold">Law enforcement</div>
+                        Under certain circumstances, the Company may be required
+                        to disclose Your Personal Data if required to do so by
+                        law or in response to valid requests by public
+                        authorities (e.g. a court or a government agency).
+                        <br />
+                        <br />
+                        <div className="bold">Other legal requirements</div>
+                        The Company may disclose Your Personal Data in the good
+                        faith belief that such action is necessary to:
+                        <br />
+                        <br />
+                        <ul>
+                          <li> - Comply with a legal obligation </li>
+                          <li>
+                            
+                            - Protect and defend the rights or property of the
+                            Company
+                          </li>
+                          <li>
+                            
+                            - Prevent or investigate possible wrongdoing in
+                            connection with the Service
+                          </li>
+                          <li>
+                            
+                            - Protect the personal safety of Users of the Service
+                            or the public
+                          </li>
+                          <li> - Protect against legal liability </li>
+                        </ul>
                       </p>
                     </div>
                   </div>
