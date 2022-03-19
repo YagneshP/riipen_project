6e6b68e5bd29d280d88c740b7fea8802fc14ec6b
raninder dhillon <raninderdhillon@gmail.com>
2022-03-19 08:57:14 -0400

    search component added

---
+++ pages/search/index.js
@@ -0,0 +1,33 @@
+import { React, useState } from "react";
+import TextField from "@mui/material/TextField";
+import List from "./searchList";
+
+
+export default function Search() {
+ 
+  const [inputText, setInputText] = useState("");
+  let inputHandler = (e) => {
+    let lowerCase = e.target.value.toLowerCase();
+    setInputText(lowerCase);
+  };
+  
+  return (
+    <div className="about">
+      <h1> Search</h1><p></p>
+      <div style={{"width":"30%"}}>
+        <TextField
+          id="outlined-basic"
+          onChange={inputHandler}
+          variant="outlined"
+          color="secondary" 
+          size="normal"
+          focused
+          fullWidth
+          label="Search"
+        />
+      </div>
+      <List input={inputText}/>
+    </div>
+  );
+}
+
