c1f0366ae44853766fb3e02bb6dcd20d8edbf247
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-03-20 00:32:58 -0400

    changes image size

--- pages/products-content/ProductItem.js
+++ pages/products-content/ProductItem.js
@@ -1,47 +1,49 @@
 /* eslint-disable */
-import Link from 'next/link';
+import Link from "next/link";
 import Image from "next/image";
 
-
-const ProductItem = ({ discount, productImage, id, brand, name, price, currentPrice }) => {
-
+const ProductItem = ({
+  discount,
+  productImage,
+  id,
+  brand,
+  name,
+  price,
+  currentPrice,
+}) => {
   return (
-    <div className="new-arrival-product">
-      <div className="product__image">
-
+    <div className='new-arrival-product'>
+      <div className='product__image'>
         <Link href={`/product/${id}`}>
           <a>
-            <img src={productImage} alt="product" />
-					</a>
-						{/* <Image 
+            <img src={productImage} alt='product' width={200} height={200} />
+          </a>
+          {/* <Image 
 							src={productImage}
 							alt="Logo"
 							width="270px"
 							height="352px"
       			/> */}
-				</Link>
+        </Link>
 
-            {discount &&
-              <p className="product__discount">{discount}%</p>
-            }
-          
-     
+        {discount && <p className='product__discount'>{discount}%</p>}
       </div>
-      
-      <div className="product__description">
+
+      <div className='product__description'>
         <h3>{brand}</h3>
         <h4>{name}</h4>
-        <div className={"product__price " + (discount ? 'product__price--discount' : '')} >
-          <h4>${ currentPrice }</h4>
-
-          {discount &&
-            <span>${ price }</span>
+        <div
+          className={
+            "product__price " + (discount ? "product__price--discount" : "")
           }
+        >
+          <h4>{currentPrice}</h4>
+
+          {discount && <span>${price}</span>}
         </div>
       </div>
     </div>
   );
 };
 
-
-export default ProductItem
+export default ProductItem;
