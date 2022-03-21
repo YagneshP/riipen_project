4732750ca3f2cfd5bb1e986ad4fbc796e8f13b90
houseofsam <samxsaid@gmail.com>
2022-03-21 15:58:16 -0400

    fix issue overriding about us page image styles
    Global selectors were used for the shipping methods and policy pages

--- components/ShippingMethod/index.js
+++ components/ShippingMethod/index.js
@@ -1,6 +1,6 @@
 const ShippingMethod = () => {
   return (
-    <div>
+    <div id="shipping-method-page">
       <div id="wrap">
         <div id="content">
           <section className="history-block padding-top-100 padding-bottom-100">
