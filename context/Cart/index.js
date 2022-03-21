a86a9b2e455ba26beae0b26df94527fc9e1ad921
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-03-21 10:02:59 -0400

    Create Cart context

---
+++ context/Cart/index.js
@@ -0,0 +1,17 @@
+import { createContext } from "react";
+
+const CartStateContext = createContext(null);
+const CartDispatchContext = createContext(null);
+
+// // cart reducer
+
+// const cartReducer (state, action) => {
+//  switch (action.type) {
+//    case SET_CART:
+
+//      break;
+
+//    default:
+//      break;
+//  }
+// }
