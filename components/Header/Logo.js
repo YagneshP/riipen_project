262d74dc6b9f373923d5e6380ff36cb24ac69aa5
houseofsam <samxsaid@gmail.com>
2022-03-21 16:19:09 -0400

    Fix links inside of nav logo and header icons

--- components/Header/Logo.js
+++ components/Header/Logo.js
@@ -1,17 +1,20 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Logo = () => {
   return (
     <div className="logo">
-      <a href="index.html">
-        <Image
-          className="img-responsive"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVUnoeoqy6iVGtzsp77gOOkotxJoScrAiktLgKEU7d4ht-vGnERSSF0VtpVq8tzk5cltg&usqp=CAU"
-          alt="Logo"
-          width="217px"
-          height="50px"
-        />
-      </a>
+      <Link href="/" passHref>
+        <a>
+          <Image
+            className="img-responsive"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVUnoeoqy6iVGtzsp77gOOkotxJoScrAiktLgKEU7d4ht-vGnERSSF0VtpVq8tzk5cltg&usqp=CAU"
+            alt="Logo"
+            width="217px"
+            height="50px"
+          />
+        </a>
+      </Link>
     </div>
   );
 };
