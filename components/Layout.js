f19ea82c0166e3b493f62a15e04d6f20ae553b8d
raninder dhillon <raninderdhillon@gmail.com>
2022-03-21 19:24:57 -0400

    cleanup

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
 /* eslint-disable */
 import Footer from './Footer';
 import Header from './Header/index';
 import NewsletterForm from './NewsletterForm';
 import Banner from './Header/Banner';
-=======
-import Footer from "./Footer";
-import Header from "./Header/index";
-import NewsletterForm from "./NewsletterForm";
-import Banner from "./Header/Banner";
->>>>>>> main
 
 const Layout = ({ children }) => {
   return (
