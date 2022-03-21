e7e356e6cfe6c6a9b9da92d5ed42f1acda70c680
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-03-21 10:20:28 -0400

    Create Cart Provider

--- context/Cart/index.js
+++ context/Cart/index.js
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 const CartStateContext = createContext(null);
 const CartDispatchContext = createContext(null);
@@ -6,23 +6,35 @@ const CartDispatchContext = createContext(null);
 //cart intialState
 
 const cartInitialState = {
-  total_items : 0,
-  total_unique_items : 0,
-  subtotal : {
-
+  total_items: 0,
+  total_unique_items: 0,
+  subtotal: {
+    formatted_with_symbol: "",
   },
-  line_items : []
-}
+  line_items: [],
+};
 
 // cart reducer
 
-const cartReducer (state, action) => {
- switch (action.type) {
-   case 'SET_CART':
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_CART": {
+      return { ...state, ...action.payload };
+    }
 
-     break;
+    default:
+      throw new Error(`Unknown action ${action.type}`);
+  }
+};
 
-   default:
-     break;
- }
-}
+// Cart Provider
+
+export default CartProvider = ({ children }) => {
+  const [cart, dispatch] = useReducer(cartReducer, cartInitialState);
+
+  return (
+    <CartStateContext value={cart}>
+      <CartDispatchContext value={dispatch}>{children}</CartDispatchContext>
+    </CartStateContext>
+  );
+};
