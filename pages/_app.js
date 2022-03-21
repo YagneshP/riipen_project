077ef6e7efb452563e00a327b5068f87c1ca159b
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-03-21 10:26:31 -0400

    Providing Cart provider to app

--- pages/_app.js
+++ pages/_app.js
@@ -5,17 +5,20 @@ import { AuthProvider } from "../context/Auth";
 import "bootstrap/dist/css/bootstrap.css";
 import { Provider } from "react-redux";
 import store from "../redux/store";
+import { CartProvider } from "../context/Cart";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <div className='wrapper'>
-        <AuthProvider>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </AuthProvider>
-      </div>
+      <CartProvider>
+        <div className='wrapper'>
+          <AuthProvider>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </AuthProvider>
+        </div>
+      </CartProvider>
     </Provider>
   );
 }
