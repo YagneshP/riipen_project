f2bfb33b3bf46163036ae9b3f68a2f86a7d5e976
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-03-20 20:11:40 -0400

    Add commerce js domain

--- next.config.js
+++ next.config.js
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
-require('dotenv').config()
+require("dotenv").config();
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
 
 module.exports = {
   images: {
-    domains: ['via.placeholder.com', 'encrypted-tbn0.gstatic.com', 'placeimg.com'],
+    domains: [
+      "via.placeholder.com",
+      "encrypted-tbn0.gstatic.com",
+      "placeimg.com",
+      "cdn.chec.io",
+    ],
   },
-}
+};
