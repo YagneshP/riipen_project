5e0df11d68aaebb9116fb24ffa91b3e46e31e776
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-03-21 14:08:04 -0400

    change subtotal to empty object

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,110 +1,121 @@
 import Image from "next/image";
 // import '../styles/cart'
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import CartItem from "./CartItem";
 import GrandTotal from "./GrandTotal";
 // import Image from 'next/image';
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 import { Link } from "@mui/material";
 import { useRouter } from "next/router";
+import { useCart } from "../../context/Cart";
 
 const Cart = () => {
-  const router = useRouter()
+  const router = useRouter();
+
+  const { line_items, subtotal } = useCart();
+  console.log("cart", line_items);
+  console.log("subtootal", subtotal);
   const handleClick = (e) => {
-    e.preventDefault()
-    router.push("/products-content")
-  }
- 
-  console.log("Hello");
-
-const cart = useSelector((state) => state.cart);
-
-
-  const getTotalPrice = () => {
-    return cart.reduce(
-      (accumulator, item) => accumulator + item.quantity * item.price,
-      0
-    );
+    e.preventDefault();
+    router.push("/products-content");
   };
-  console.log("gettotal",getTotalPrice());
+
+  // console.log("Hello");
+
+  // const cart = useSelector((state) => state.cart);
+
+  // const getTotalPrice = () => {
+  //   return cart.reduce(
+  //     (accumulator, item) => accumulator + item.quantity * item.price,
+  //     0
+  //   );
+  // };
+  // console.log("gettotal", getTotalPrice());
   return (
-   
     <div>
       <main>
-        <section className="padding-top-100 padding-bottom-100 pages-in chart-page">
-          <div className="main">
-
+        <section className='padding-top-100 padding-bottom-100 pages-in chart-page'>
+          <div className='main'>
             {/* <!-- Payments Steps --> */}
-            <div className="shopping-cart text-center">
-              <div className="cart-head">
-                <ul className="row">
+            <div className='shopping-cart text-center'>
+              <div className='cart-head'>
+                <ul className='row'>
                   {/* <!-- PRODUCTS --> */}
-                  <li className="col-sm-2 text-left">
+                  <li className='col-sm-2 text-left'>
                     <h6>PRODUCTS</h6>
                   </li>
                   {/* <!-- NAME --> */}
-                  <li className="col-sm-4 text-left">
+                  <li className='col-sm-4 text-left'>
                     <h6>NAME</h6>
                   </li>
                   {/* <!-- PRICE --> */}
-                  <li className="col-sm-2">
+                  <li className='col-sm-2'>
                     <h6>PRICE</h6>
                   </li>
                   {/* <!-- QTY --> */}
-                  <li className="col-sm-1">
+                  <li className='col-sm-1'>
                     <h6>QTY</h6>
                   </li>
 
                   {/* <!-- TOTAL PRICE --> */}
-                  <li className="col-sm-2">
+                  <li className='col-sm-2'>
                     <h6>TOTAL</h6>
                   </li>
-                  <li className="col-sm-1"> </li>
+                  <li className='col-sm-1'> </li>
                 </ul>
               </div>
               {/* Print data  */}
-              {cart.map(item => (
+              {line_items.map((item) => (
                 <CartItem
                   key={item.id}
                   id={item.id}
                   name={item.name}
-                  brand={item.brand}
-                  price={item.price}
+                  // brand={item.brand}
+                  line_total={item.line_total.formatted_with_symbol}
+                  price={item.price.formatted_with_symbol}
                   quantity={item.quantity}
-                  image={"https://via.placeholder.com/90x65.png"}
-
+                  image={item.image.url}
                 />
               ))}
-              
-              
             </div>
           </div>
         </section>
         {/* <!--======= PAGES INNER =========--> */}
-        <section className="padding-top-100 padding-bottom-100 light-gray-bg shopping-cart small-cart">
-          <div className="main">
-
+        <section className='padding-top-100 padding-bottom-100 light-gray-bg shopping-cart small-cart'>
+          <div className='main'>
             {/* <!-- SHOPPING INFORMATION --> */}
-            <div className="cart-ship-info margin-top-0">
-              <div className="row">
-
+            <div className='cart-ship-info margin-top-0'>
+              <div className='row'>
                 {/* <!-- DISCOUNT CODE --> */}
-                <div className="col-sm-7">
+                <div className='col-sm-7'>
                   <h6>DISCOUNT CODE</h6>
                   <form>
-                    <input type="text" placeholder="ENTER YOUR CODE IF YOU HAVE ONE" />
-                    <button type="submit" className="btn btn-small btn-dark">APPLY CODE</button>
+                    <input
+                      type='text'
+                      placeholder='ENTER YOUR CODE IF YOU HAVE ONE'
+                    />
+                    <button type='submit' className='btn btn-small btn-dark'>
+                      APPLY CODE
+                    </button>
                   </form>
-                  <div className="coupn-btn"> <a onClick={handleClick} className="btn">continue shopping</a> <a href="#." className="btn">Checkout</a> </div>
+                  <div className='coupn-btn'>
+                    {" "}
+                    <a onClick={handleClick} className='btn'>
+                      continue shopping
+                    </a>{" "}
+                    <a href='#.' className='btn'>
+                      Checkout
+                    </a>{" "}
+                  </div>
                 </div>
-                <div className="col-sm-5">
+                <div className='col-sm-5'>
                   <h6>GRAND TOTAL</h6>
-                  <div className="grand-total">
-                    <div className="order-detail"></div>
-                   
-                    {  cart.map(item => (
+                  <div className='grand-total'>
+                    <div className='order-detail'></div>
+
+                    {line_items.map((item) => (
                       <GrandTotal
                         key={item.id}
                         id={item.id}
@@ -112,24 +123,20 @@ const cart = useSelector((state) => state.cart);
                         price={item.price}
                         quntity={item.quantity}
                       />
-                      
                     ))}
 
                     {/* <!-- SUB TOTAL --> */}
-                    <p className="all-total">TOTAL COST <span> ${getTotalPrice()}</span></p>
+                    <p className='all-total'>
+                      TOTAL COST <span> ${subtotal.formatted_with_symbol}</span>
+                    </p>
                   </div>
                 </div>
               </div>
-
             </div>
-        
-        </div>
-      </section>
-
-
-    </main>
-    </div >
-    
-  )
-}
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+};
 export default Cart;
