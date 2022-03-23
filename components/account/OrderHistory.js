11454424a4e3dc008c3782bb018081123e477dbf
houseofsam <samxsaid@gmail.com>
2022-03-22 23:57:57 -0400

    create minimalistic order history component with table set up

---
+++ components/account/OrderHistory.js
@@ -0,0 +1,36 @@
+const OrderHistory = () => {
+  return (
+    <table className="order-history-table">
+      <thead>
+        <tr>
+          <th className="order-num-col">Order Number</th>
+          <th className="order-items-col">Order Items</th>
+          <th className="order-total-col">Total</th>
+        </tr>
+      </thead>
+      <tr>
+        <td><a href="">14</a></td>
+        <td>
+          <ul>
+            <li>D&G</li>
+            <li>YSL</li>
+          </ul>
+        </td>
+        <td>$280</td>
+      </tr>
+      <tr>
+        <td><a href="">17</a></td>
+        <td>
+          <ul>
+            <li>Gucci</li>
+            <li>Chanel</li>
+            <li>Armani</li>
+          </ul>
+        </td>
+        <td>$280</td>
+      </tr>
+    </table>
+  );
+};
+
+export default OrderHistory;
