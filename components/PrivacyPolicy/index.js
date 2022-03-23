b52f6b1b84315ff60f8aca48be6b4fc07029a508
tofi-124 <tofikahm@yahoo.com>
2022-03-23 12:30:36 -0400

    class name change

--- components/PrivacyPolicy/index.js
+++ components/PrivacyPolicy/index.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const PrivacyPolicy = () => {
   return (
-    <div id="privacy-policy-page">
+    <div id="privacy-shipping">
       <div id="wrap">
         <div id="content">
           <section className="history-block padding-top-100 padding-bottom-100">
