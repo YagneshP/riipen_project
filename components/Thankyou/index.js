cc9f9818490f7dac8f54162672be92a37d1d6d48
tofi-124 <tofikahm@yahoo.com>
2022-03-23 12:29:23 -0400

    simple static thank you page

---
+++ components/Thankyou/index.js
@@ -0,0 +1,34 @@
+const Thankyou = () => {
+  return (
+    <div>
+      <section className="contact padding-top-100 padding-bottom-100">
+        <div className="container">
+          <div className="contact-form">
+            <p>
+              <h5> Thank You, Your Order has been Submitted!</h5> <br />
+              <h4>
+                Here is your order number for reference:
+                <hr />
+                <hr />
+                Order-id: <br />
+                Order-detail: <br />
+                <hr />
+              </h4>
+              <br />
+            
+            </p>
+            <h4>
+              Company Name <br />
+              Street No. 12, Newyork 12,
+              <br />
+              MD - 123, USA.
+              <br /> 1.800.123.456789 <br />
+              info@ecoshop.com
+            </h4>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+export default Thankyou;
