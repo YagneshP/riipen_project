a129d1103a13dab7b7854def8c2894ca95e92490
tofi-124 <tofikahm@yahoo.com>
2022-03-24 20:11:37 -0400

    jsx syntax fixed

--- components/Checkout/index.js
+++ components/Checkout/index.js
@@ -1,27 +1,21 @@
 const Checkout = () => {
-  
   return (
     <div id="content">
       <section className="chart-page padding-top-100 padding-bottom-100">
         <div className="container">
-          {/* Payments Steps */}
           <div className="shopping-cart">
-            {/* SHOPPING INFORMATION */}
             <div className="cart-ship-info">
               <div className="row">
-                {/* ESTIMATE SHIPPING & TAX */}
                 <div className="col-sm-7">
                   <h6>BILLING DETAILS</h6>
                   <form>
                     <ul className="row">
-                      {/* Name */}
                       <li className="col-md-6">
                         <label>
                           *FIRST NAME
                           <input type="text" name="first-name" />
                         </label>
                       </li>
-                      {/* LAST NAME */}
                       <li className="col-md-6">
                         <label>
                           *LAST NAME
@@ -29,54 +23,46 @@ const Checkout = () => {
                         </label>
                       </li>
                       <li className="col-md-6">
-                        {/* COMPANY NAME */}
                         <label>
                           COMPANY NAME
                           <input type="text" name="company" />
                         </label>
                       </li>
                       <li className="col-md-6">
-                        {/* ADDRESS */}
                         <label>
                           *ADDRESS
                           <input type="text" name="address" />
                         </label>
                       </li>
-                      {/* TOWN/CITY */}
                       <li className="col-md-6">
                         <label>
                           *TOWN/CITY
                           <input type="text" name="town" />
                         </label>
                       </li>
-                      {/* COUNTRY */}
                       <li className="col-md-6">
                         <label>
                           COUNTRY
                           <input type="text" name="country-state" />
                         </label>
                       </li>
-                      {/* EMAIL ADDRESS */}
                       <li className="col-md-6">
                         <label>
                           *EMAIL ADDRESS
                           <input type="text" name="country-state" />
                         </label>
                       </li>
-                      {/* PHONE */}
                       <li className="col-md-6">
                         <label>
                           *PHONE
                           <input type="text" name="postal-code" />
                         </label>
                       </li>
-                      {/* PHONE */}
                       <li className="col-md-6">
                         <button type="submit" className="button-chk">
                           Continue
                         </button>
                       </li>
-                      {/* CREATE AN ACCOUNT */}
                       <li className="col-md-6">
                         <div className="checkbox margin-0 margin-top-20">
                           <input
@@ -91,18 +77,15 @@ const Checkout = () => {
                       </li>
                     </ul>
                   </form>
-                  {/* SHIPPING info */}
                   <h6 className="margin-top-50">SHIPPING info</h6>
                   <form>
                     <ul className="row">
-                      {/* Name */}
                       <li className="col-md-6">
                         <label>
                           *FIRST NAME
                           <input type="text" name="first-name" />
                         </label>
                       </li>
-                      {/* LAST NAME */}
                       <li className="col-md-6">
                         <label>
                           *LAST NAME
@@ -110,48 +93,41 @@ const Checkout = () => {
                         </label>
                       </li>
                       <li className="col-md-6">
-                        {/* COMPANY NAME */}
                         <label>
                           COMPANY NAME
                           <input type="text" name="company" />
                         </label>
                       </li>
                       <li className="col-md-6">
-                        {/* ADDRESS */}
                         <label>
                           *ADDRESS
                           <input type="text" name="address" />
                         </label>
                       </li>
-                      {/* TOWN/CITY */}
                       <li className="col-md-6">
                         <label>
                           *TOWN/CITY
                           <input type="text" name="town" />
                         </label>
                       </li>
-                      {/* COUNTRY */}
                       <li className="col-md-6">
                         <label>
                           COUNTRY
                           <input type="text" name="country-state" />
                         </label>
                       </li>
-                      {/* EMAIL ADDRESS */}
                       <li className="col-md-6">
                         <label>
                           *EMAIL ADDRESS
                           <input type="text" name="country-state" />
                         </label>
                       </li>
-                      {/* PHONE */}
                       <li className="col-md-6">
                         <label>
                           *PHONE
                           <input type="text" name="postal-code" />
                         </label>
                       </li>
-                      {/* PHONE */}
                       <li className="col-md-6">
                         <button type="submit" className=" button-chk">
                           SUBMIT
@@ -160,7 +136,6 @@ const Checkout = () => {
                     </ul>
                   </form>
                 </div>
-                {/* SUB TOTAL */}
                 <div className="col-sm-5">
                   <h6>YOUR ORDER</h6>
                   <div className="order-place">
@@ -174,7 +149,7 @@ const Checkout = () => {
                       <p>
                         WOOD SPOON <span> $139</span>
                       </p>
-                      {/* SUB TOTAL */}
+
                       <p className="all-total">
                         TOTAL COST <span> $998</span>
                       </p>
@@ -202,9 +177,7 @@ const Checkout = () => {
                             />
                             <label htmlFor="checkbox3-4">
                               I’VE READ AND ACCEPT THE
-                              <span className="color">
-                                TERMS &amp; CONDITIONS
-                              </span>
+                              <span className="color">TERMS & CONDITIONS</span>
                             </label>
                           </div>
                         </li>
