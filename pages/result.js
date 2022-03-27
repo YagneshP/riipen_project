318a15d371d8d2a20b18ded88ff0fae8de06fae8
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-03-27 00:02:29 -0400

    Add result page

---
+++ pages/result.js
@@ -0,0 +1,28 @@
+import { useRouter } from "next/router";
+import useSWR from "swr";
+function Result() {
+  const router = useRouter();
+  const { session_id } = router.query;
+  // Fetch CheckoutSession from static page via
+  // https://nextjs.org/docs/basic-features/data-fetching#static-generation
+  const { data, error } = useSWR(
+    router.query.session_id
+      ? `/api/checkout_sessions/${router.query.session_id}`
+      : null,
+    (url) => fetch(url).then((res) => res.json)
+  );
+
+  if (error) return <div>failed to load</div>;
+
+  return (
+    <div className='page-container'>
+      <h1>Checkout Payment Result</h1>
+      {/* <h2>Status: {data?.payment_intent?.status ?? "loading..."}</h2> */}
+      <h3>CheckoutSession response:</h3>
+      <pre>{data ? JSON.stringify(data, null, 2) : "loading..."} </pre>
+    </div>
+  );
+  // return <div>{session_id}</div>;
+}
+
+export default Result;
