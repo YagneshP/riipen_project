59a3552060f41aeee722c9fbd25fb3ec656e3a0f
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-03-26 23:27:58 -0400

    Renedering Element form instead Checkout

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,9 +1,11 @@
 import Checkout from "../components/Checkout";
+import ElementForm from "../components/PaymentCheckout/ElementForm";
 
 const Layout = () => {
   return (
     <div className='content'>
       <Checkout />
+      {/* <ElementForm stripe={stripePromise} /> */}
     </div>
   );
 };
