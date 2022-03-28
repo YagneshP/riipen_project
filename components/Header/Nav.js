e54635aa437094732a2bffd5e45b0b9161c50ba1
houseofsam <samxsaid@gmail.com>
2022-03-28 09:29:39 -0400

    Start implementing sub-menu dropdown on hover using framer motion

--- components/Header/Nav.js
+++ components/Header/Nav.js
@@ -3,6 +3,8 @@ import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import Link from "next/link";
 import { useSelector } from "react-redux";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 const Nav = () => {
   // Selecting cart from global state
@@ -13,22 +15,68 @@ const Nav = () => {
     return cart.reduce((accumulator, item) => accumulator + item.quantity, 0);
   };
   console.log("getItemsCount", getItemsCount());
+
+  // framer-motion sub-menu state/config
+  const [isHover, toggleHover] = useState(false);
+  const toggleHoverMenu = () => {
+    toggleHover(!isHover);
+  };
+  const [isMouse, toggleMouse] = useState(false);
+  const toggleMouseMenu = () => {
+    toggleMouse(!isMouse);
+  };
+  const subMenuAnimate = {
+    enter: {
+      opacity: 1,
+      rotateX: 0,
+      transition: {
+        duration: 0.5,
+      },
+      display: "block",
+    },
+    exit: {
+      opacity: 0,
+      rotateX: -15,
+      transition: {
+        duration: 0.5,
+        delay: 0.3,
+      },
+      transitionEnd: {
+        display: "none",
+      },
+    },
+  };
+
   return (
     <ul className="nav">
       <li>
         <Link href="/">Home</Link>
       </li>
-      <li>
-        <Link href="/product">Store</Link>
-      </li>
-      <ul className="sub-menu">
+      <motion.div
+        className="menu-item"
+        onHoverStart={toggleHoverMenu}
+        onHoverEnd={toggleHoverMenu}
+      >
         <li>
-          <Link href="#">Men&apos;s Frangrances</Link>
+          <Link href="/product">Store</Link>
         </li>
-        <li>
-          <Link href="#">Women&apos;s Frangrances</Link>
-        </li>
-      </ul>
+        <motion.div
+          className="sub-menu"
+          initial="exit"
+          animate={isHover ? "enter" : "exit"}
+          variants={subMenuAnimate}
+        >
+          <div className="sub-menu-background" />
+          <ul className="sub-menu-container">
+            <li>
+              <Link href="#" className="sub-menu-item">Men&apos;s Frangrances</Link>
+            </li>
+            <li>
+              <Link href="#" className="sub-menu-item">Women&apos;s Frangrances</Link>
+            </li>
+          </ul>
+        </motion.div>
+      </motion.div>
       <li>
         <Link href="/about">About Us</Link>
       </li>
