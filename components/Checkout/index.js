da678831bc585d9021d45ef8795f49012f48621c
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-03-29 13:56:54 -0400

    importing Billing and shopping form in main Checkoutform

--- components/Checkout/index.js
+++ components/Checkout/index.js
@@ -1,5 +1,7 @@
 import { useCart } from "../../context/Cart";
+import BillingInfoForm from "./BillingInfoForm";
 import OrderDetail from "./OrderDetail";
+import ShippingInfoForm from "./ShippingInfoForm";
 
 const Checkout = () => {
   const { line_items, subtotal } = useCart();
@@ -12,133 +14,9 @@ const Checkout = () => {
               <div className='row'>
                 <div className='col-sm-7'>
                   <h6>BILLING DETAILS</h6>
-                  <form>
-                    <ul className='row'>
-                      <li className='col-md-6'>
-                        <label>
-                          *FIRST NAME
-                          <input type='text' name='first-name' />
-                        </label>
-                      </li>
-                      <li className='col-md-6'>
-                        <label>
-                          *LAST NAME
-                          <input type='text' name='last-name' />
-                        </label>
-                      </li>
-                      <li className='col-md-6'>
-                        <label>
-                          COMPANY NAME
-                          <input type='text' name='company' />
-                        </label>
-                      </li>
-                      <li className='col-md-6'>
-                        <label>
-                          *ADDRESS
-                          <input type='text' name='address' />
-                        </label>
-                      </li>
-                      <li className='col-md-6'>
-                        <label>
-                          *TOWN/CITY
-                          <input type='text' name='town' />
-                        </label>
-                      </li>
-                      <li className='col-md-6'>
-                        <label>
-                          COUNTRY
-                          <input type='text' name='country-state' />
-                        </label>
-                      </li>
-                      <li className='col-md-6'>
-                        <label>
-                          *EMAIL ADDRESS
-                          <input type='text' name='country-state' />
-                        </label>
-                      </li>
-                      <li className='col-md-6'>
-                        <label>
-                          *PHONE
-                          <input type='text' name='postal-code' />
-                        </label>
-                      </li>
-                      <li className='col-md-6'>
-                        <button type='submit' className='button-chk'>
-                          Continue
-                        </button>
-                      </li>
-                      <li className='col-md-6'>
-                        <div className='checkbox margin-0 margin-top-20'>
-                          <input
-                            id='checkbox1'
-                            className='styled'
-                            type='checkbox'
-                          />
-                          <label htmlFor='checkbox1'>
-                            Ship to a different address
-                          </label>
-                        </div>
-                      </li>
-                    </ul>
-                  </form>
+                  <BillingInfoForm />
                   <h6 className='margin-top-50'>SHIPPING info</h6>
-                  <form>
-                    <ul className='row'>
-                      <li className='col-md-6'>
-                        <label>
-                          *FIRST NAME
-                          <input type='text' name='first-name' />
-                        </label>
-                      </li>
-                      <li className='col-md-6'>
-                        <label>
-                          *LAST NAME
-                          <input type='text' name='last-name' />
-                        </label>
-                      </li>
-                      <li className='col-md-6'>
-                        <label>
-                          COMPANY NAME
-                          <input type='text' name='company' />
-                        </label>
-                      </li>
-                      <li className='col-md-6'>
-                        <label>
-                          *ADDRESS
-                          <input type='text' name='address' />
-                        </label>
-                      </li>
-                      <li className='col-md-6'>
-                        <label>
-                          *TOWN/CITY
-                          <input type='text' name='town' />
-                        </label>
-                      </li>
-                      <li className='col-md-6'>
-                        <label>
-                          COUNTRY
-                          <input type='text' name='country-state' />
-                        </label>
-                      </li>
-                      <li className='col-md-6'>
-                        <label>
-                          *EMAIL ADDRESS
-                          <input type='text' name='country-state' />
-                        </label>
-                      </li>
-                      <li className='col-md-6'>
-                        <label>
-                          *PHONE
-                          <input type='text' name='postal-code' />
-                        </label>
-                      </li>
-                      <li className='col-md-6'>
-                        <button type='submit' className=' button-chk'>
-                          SUBMIT
-                        </button>
-                      </li>
-                    </ul>
-                  </form>
+                  <ShippingInfoForm />
                 </div>
                 <div className='col-sm-5'>
                   <h6>YOUR ORDER</h6>
