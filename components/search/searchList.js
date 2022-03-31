8ac9310b5accec1d4c60c0317ca8bf8df95bd081
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-03-31 11:40:44 -0400

    Remove unwanted props.product[0]

--- components/search/searchList.js
+++ components/search/searchList.js
@@ -1,49 +1,39 @@
-/* eslint-disable */
-
-import { FiCornerDownLeft } from 'react-icons/fi';
-import ProductItem from '../Product/ProductItem';
+import ProductItem from "../Product/ProductItem";
 
 const List = (props) => {
+  const filteredData = props.products.filter((el) => {
+    if (props.input === "") {
+      return el.id;
+    } else {
+      return el.name.toLowerCase().includes(props.input);
+      //other search options
+      // return ( el.brand.toLowerCase().includes(props.input)||
+      // 					el.category.toLowerCase()===props.input);
+    }
+  });
 
-	console.log("props prods", props.products);
-	console.log("props img....", props.products[0].image);
-	const filteredData = props.products.filter((el) => {
-	
-		if (props.input === '') {
-				return el.id;
-		}
-
-		else {
-			return el.name.toLowerCase().includes(props.input);
-			//other search options
-			// return ( el.brand.toLowerCase().includes(props.input)||
-			// 					el.category.toLowerCase()===props.input);
-		}
-})
-console.log("filtereddata", filteredData[0].image.url);
-
-	return (
-		<>
-			<section className="products">
-				<h1> Fragrances</h1><hr/>
-				<div className="new-arrival-products">
-
-					{filteredData.map(product => (
-						<ProductItem
-						key={product.id}
-						id={product.id}
-						permalink={product.permalink}
-						price={product.price.formatted_with_symbol}
-						currentPrice={product.price.formatted_with_symbol}
-						Image={product.image.url}
-						name={product.name}
-						brand={product.name}
-					/>
-					))}
-				</div>
-			</section>
-		</>
-	);
+  return (
+    <>
+      <section className='products'>
+        <h1> Fragrances</h1>
+        <hr />
+        <div className='new-arrival-products'>
+          {filteredData.map((product) => (
+            <ProductItem
+              key={product.id}
+              id={product.id}
+              permalink={product.permalink}
+              price={product.price.formatted_with_symbol}
+              currentPrice={product.price.formatted_with_symbol}
+              Image={product.image.url}
+              name={product.name}
+              brand={product.name}
+            />
+          ))}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default List;
